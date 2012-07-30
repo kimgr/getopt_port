6a8bb027ed90caba73daacd2457fffb4e5b9287c
Kim Grasman <kim.grasman@gmail.com>
2012-07-30 10:23:45 +0200

    80-character line length.

--- getopt.c
+++ getopt.c
@@ -9,14 +9,18 @@ const int optional_argument = 2;
 
 char* optarg;
 int optopt;
-int optind = 1; /* The variable optind [...] shall be initialized to 1 by the system */
+/* The variable optind [...] shall be initialized to 1 by the system */
+int optind = 1;
 int opterr;
 
 static char* optcursor = NULL;
 
-/* Implemented based on http://pubs.opengroup.org/onlinepubs/000095399/functions/getopt.html
-   and http://www.kernel.org/doc/man-pages/online/pages/man3/getopt.3.html for optional arguments. 
-   Other GNU extensions are purely accidental. */
+/* Implemented based on [1] and [2] for optional arguments. 
+   Other GNU extensions are purely accidental. 
+   
+   [1] http://pubs.opengroup.org/onlinepubs/000095399/functions/getopt.html
+   [2] http://www.kernel.org/doc/man-pages/online/pages/man3/getopt.3.html
+   */
 int getopt(int argc, char* const argv[], const char* optstring)
 {
   int optchar = -1;
@@ -30,19 +34,23 @@ int getopt(int argc, char* const argv[], const char* optstring)
   if (optind >= argc)
     goto no_more_optchars;
 
-  /* If, when getopt() is called argv[optind] is a null pointer, getopt() shall return -1 without changing optind. */
+  /* If, when getopt() is called argv[optind] is a null pointer, getopt() 
+     shall return -1 without changing optind. */
   if (argv[optind] == NULL)
     goto no_more_optchars;
 
-  /* If, when getopt() is called *argv[optind]  is not the character '-', getopt() shall return -1 without changing optind. */
+  /* If, when getopt() is called *argv[optind]  is not the character '-', 
+     getopt() shall return -1 without changing optind. */
   if (*argv[optind] != '-')
     goto no_more_optchars;
 
-  /* If, when getopt() is called argv[optind] points to the string "-", getopt() shall return -1 without changing optind. */
+  /* If, when getopt() is called argv[optind] points to the string "-", 
+     getopt() shall return -1 without changing optind. */
   if (strcmp(argv[optind], "-") == 0)
     goto no_more_optchars;
 
-  /* If, when getopt() is called argv[optind] points to the string "--", getopt() shall return -1 after incrementing optind. */
+  /* If, when getopt() is called argv[optind] points to the string "--", 
+     getopt() shall return -1 after incrementing optind. */
   if (strcmp(argv[optind], "--") == 0)
   {
     ++optind;
@@ -54,30 +62,35 @@ int getopt(int argc, char* const argv[], const char* optstring)
 
   optchar = *optcursor;
 
-  /*  The getopt() function shall return the next option character (if one is found) from argv
-      that num_matches a character in optstring, if there is one that num_matches */
+  /* The getopt() function shall return the next option character (if one is 
+     found) from argv that matches a character in optstring, if there is 
+     one that matches */
   optdecl = strchr(optstring, optchar);
   if (optdecl)
   {
-    /* [I]f a character is followed by a colon, the option takes an argument */
+    /* [I]f a character is followed by a colon, the option takes an
+       argument */
     if (optdecl[1] == ':')
     {
       optarg = ++optcursor;
       if (*optarg == '\0')
       {
         /* GNU extension: Two colons mean an option takes an
-           optional arg; if there is text in the current argv-element (i.e., in the same
-           word as the option name itself, for example, "-oarg"), then it is returned in
-           optarg, otherwise optarg is set to zero. */
+           optional arg; if there is text in the current argv-element 
+           (i.e., in the same word as the option name itself, for example, 
+           "-oarg"), then it is returned in optarg, otherwise optarg is set
+           to zero. */
         if (optdecl[2] != ':')
         {
-          /* If the option was the last character in the string pointed to by an element of argv, 
-              then optarg shall contain the next element of argv, and optind shall be incremented 
-              by 2. If the resulting value of optind is greater than argc, this indicates a missing 
-              option-argument, and getopt() shall return an error indication.
+          /* If the option was the last character in the string pointed to by
+             an element of argv, then optarg shall contain the next element
+             of argv, and optind shall be incremented by 2. If the resulting
+             value of optind is greater than argc, this indicates a missing 
+             option-argument, and getopt() shall return an error indication.
 
-              Otherwise, optarg shall point to the string following the option character in that 
-              element of argv, and optind shall be incremented by 1.
+             Otherwise, optarg shall point to the string following the
+             option character in that element of argv, and optind shall be
+             incremented by 1.
           */
           if (optind < argc - 1)
           {
@@ -86,8 +99,13 @@ int getopt(int argc, char* const argv[], const char* optstring)
           }
           else
           {
-            /* If it detects a missing option-argument, it shall return the colon character ( ':' ) if the first character of optstring was a colon, or a question-mark character ( '?' ) otherwise. */
-            /* [...] getopt() shall set the variable optopt to the option character that caused the error. */
+            /* If it detects a missing option-argument, it shall return the 
+               colon character ( ':' ) if the first character of optstring
+               was a colon, or a question-mark character ( '?' ) otherwise.
+            */
+
+            /* [...] getopt() shall set the variable optopt to the option 
+               character that caused the error. */
             optopt = optchar;
             optchar = (optstring[0] == ':') ? ':' : '?';
           }
@@ -105,8 +123,11 @@ int getopt(int argc, char* const argv[], const char* optstring)
   }
   else
   {
-    /* If getopt() encounters an option character that is not contained in optstring, it shall return the question-mark ( '?' ) character. */
-    /* [...] getopt() shall set the variable optopt to the option character that caused the error. */
+    /* If getopt() encounters an option character that is not contained in 
+       optstring, it shall return the question-mark ( '?' ) character. */
+
+    /* [...] getopt() shall set the variable optopt to the option character 
+       that caused the error. */
     optopt = optchar;
     optchar = '?';
   }
@@ -153,13 +174,15 @@ int getopt_long(int argc, char* const argv[],
   if (num_matches != 1)
     return '?';
 
-  /* If longindex is not NULL, it points to a variable which is set to the index of the long option relative to longopts */
+  /* If longindex is not NULL, it points to a variable which is set to the
+     index of the long option relative to longopts */
   if (longindex)
     *longindex = (match - longopts);
 
   /* If flag is NULL, then getopt_long() shall return val. 
-    Otherwise, getopt_long() returns 0, and flag shall point to a variable which shall be 
-    set to val if the option is found, but left unchanged if the option is not found. */
+     Otherwise, getopt_long() returns 0, and flag shall point to a variable
+     which shall be set to val if the option is found, but left unchanged if
+     the option is not found. */
   if (match->flag)
     *(match->flag) = match->val;
 
