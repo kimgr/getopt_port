18b6f62bfca3e043a29634e493ec60fcca749205
Kim Grasman <kim.grasman@gmail.com>
2012-08-05 12:01:34 +0200

    Simplify testfx now that most of it is in .cpp file.

--- testfx.cpp
+++ testfx.cpp
@@ -3,19 +3,13 @@
 
 typedef std::vector<std::pair< const char*, testfunction> > test_vector;
 
-const test_vector& tests();
-
-static test_vector& mutable_tests() {
+static test_vector& tests() {
   static test_vector tests;
   return tests;
 }
 
-const test_vector& tests() {
-  return mutable_tests();
-}
-
 void register_test(const char* name, testfunction f) {
-  mutable_tests().push_back(std::make_pair(name, f));
+  tests().push_back(std::make_pair(name, f));
 }
 
 int run_tests() {
